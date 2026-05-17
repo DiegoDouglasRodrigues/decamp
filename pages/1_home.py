e6784fdfb40c845bf7756e641f46158a074de75a
DiegoDouglasRodrigues <diego.rodrigues2013dr@gmail.com>
2026-05-17 16:32:01 -0300

    1 commit 17-05

---
+++ pages/1_home.py
@@ -0,0 +1,54 @@
+import time
+
+import streamlit as st
+import datetime
+
+
+st.title('essa eh a minha pagina inicial')
+
+st.write('___________________________________________________________________________________________________________________________________________________________')
+
+
+# --- LINHA 1 ---
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    if st.button("🏠 Home"):
+        st.write(" atualizando Pagina")
+        time.sleep(2)
+        st.switch_page("pages/1_home.py")
+
+
+
+with col2:
+    if st.button("🔍 Operacional"):
+        st.switch_page("pages/2_operacional.py")
+
+
+with col3:
+    if st.button("⚙️ Relatorios"):
+        st.switch_page("pages/3_relatorios.py")
+
+
+# Espaço opcional entre linhas
+st.write("")
+
+
+# --- LINHA 2 ---
+col4, col5, col6 = st.columns(3)
+
+with col4:
+    if st.button("👤 Desenvovimento"):
+        st.write("Desenvovimento")
+
+with col5:
+    if st.button("📊 Desenvovimento"):
+        st.write("Desenvovimento")
+
+with col6:
+    if st.button("❓ Desenvovimento"):
+        st.write("Desenvovimento")
+
+
+
+#streamlit run app.py

8d7dd0d629ab4bf8274d5f7c3d145664c82c5d71
DiegoDouglasRodrigues <diego.rodrigues2013dr@gmail.com>
2026-05-24 18:28:47 -0300

    Atualizacao banco e home

--- pages/1_home.py
+++ pages/1_home.py
@@ -4,7 +4,7 @@ import streamlit as st
 import datetime
 
 
-st.title('essa é a minha pagina inicial')
+st.title('essa é a minha pagina inicial!!!!')
 
 st.write('___________________________________________________________________________________________________________________________________________________________')
 

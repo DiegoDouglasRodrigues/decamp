28c6ce5067b37473ed4fb963a081fc5389303a37
DiegoDouglasRodrigues <diego.rodrigues2013dr@gmail.com>
2026-05-24 21:35:01 -0300

    atualizando git

--- pages/1_home.py
+++ pages/1_home.py
@@ -5,6 +5,8 @@ import datetime
 
 
 st.title('essa é a minha pagina inicial!!!!')
+st.title('essa é a minha pagina inicial!!!!')
+st.title('essa é a minha pagina inicial!!!!')
 
 st.write('___________________________________________________________________________________________________________________________________________________________')
 
